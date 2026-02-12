98ae321496f1e7976c391741292797d606c17e40
void-0263 <ffire09361@gmail.com>
2026-02-12 22:05:18 +0530

    day2upload

---
+++ demo/lib/tempCodeRunnerFile.dart
@@ -0,0 +1,14 @@
+
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'RIS__',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //cd
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorSc
